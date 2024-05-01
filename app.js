100011a62514a8ebda9b15fc50a64dd7261833c5
Luis Henrique Ries <prof.ries@gmail.com>
2024-04-30 22:16:53 -0300

    Exercicio 3

--- app.js
+++ app.js
@@ -1,27 +1,85 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json()) // for parsing application/json
+
+
+let listaProdutos = [
+    {
+        id: 1,
+        nome: "arroz",
+        categoria: "alimento",
+        preco: 5.80
+    },
+    {
+        id: 2,
+        nome: "leite",
+        categoria: "bebida",
+        preco: 4.25
+    }
+];
+
+let produto2 = {
+    id: 2,
+    nome: "leite",
+    categoria: "bebida",
+    preco: 4.25
+};
+
+
 app.get("/produtos", (req, res) => {
-    res.send("Listando Produtos");
+    res.json(listaProdutos)
 })
 
 app.post("/produtos", (req, res) => {
-    res.send("Inserindo Produto");
+    let produto = req.body;
+    if(produto && produto.nome && produto.preco) {
+        produto.id=3;
+        res.status(201).json(produto);
+    }
+    else {
+        res.status(400).json({erro:"Nome e preco obrigatorios"});
+    }
 })
 
 app.get("/produtos/:id", (req, res) => {
     const id = req.params.id;
-    res.send("Buscando o produto com id "+id);
+    if(id == 2) {
+        res.json(produto2);
+    }
+    else{ 
+        res.status(404).json({erro: "Produto nao encontrado"})
+    }
 })
 
 app.put("/produtos/:id", (req, res) => {
     const id = req.params.id;
-    res.send("Atualizando o produto com id "+id);
+    let produto = req.body;
+
+    if(id == 2) {
+        if(produto && produto.nome && produto.preco) {
+            //Só retorna o produto, mas aqui faria a logica de atualizar
+            res.json(produto);
+        }
+        else {
+            res.status(400).json({erro:"Nome e preco obrigatorios"});
+        }
+    }
+    else{ 
+        res.status(404).json({erro: "Produto nao encontrado"})
+    }
+
 })
 
 app.delete("/produtos/:id", (req, res) => {
     const id = req.params.id;
-    res.send("Deletando o produto com id "+id);
+    if(id == 2) {
+        //Logica semelhante ao Buscar Por Id, mas teria que deletar
+        res.json(produto2);
+    }
+    else{ 
+        res.status(404).json({erro: "Produto nao encontrado"})
+    }
 })
 
 
